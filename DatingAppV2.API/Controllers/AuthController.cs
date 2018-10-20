f071ac44e5642280f7f21f881eeb0ab7b65a1388
Sagar Tanivade <Sagar.tanivade@gmail.com>
2018-10-20 20:08:26 +0530

    Revert "Controller commits"
    This reverts commit 9e5ced26866b7fd636a03e75c0b1713efd4d896b.

--- DatingAppV2.API/Controllers/AuthController.cs
+++ DatingAppV2.API/Controllers/AuthController.cs
@@ -1,6 +1,4 @@
-using System.Threading.Tasks;
 using DatingAppV2.API.Data;
-using DatingAppV2.API.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatingAppV2.API.Controllers
@@ -9,29 +7,11 @@ namespace DatingAppV2.API.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        private readonly IAuthRepository _repo;
+        private readonly IAuthRepository _repos;
         public AuthController(IAuthRepository repo)
         {
-            _repo = repo;
-        }
-
-        [HttpPost("register")]
-
-        public async Task<IActionResult> Register(string username, string password)
-        {
-            //validate request
-            username = username.ToLower();
-            if(await _repo.UserExists(username))
-            return BadRequest("Username already exists");
-
-            var userToCreate = new User
-            {
-              Username = username
-            };
-            var createdUser = await _repo.Register(userToCreate, password);
-            return StatusCode(201);
+            _repos = repo;
         }
         
     }
-
 }
--- DatingAppV2.API/Dtos/UserForRegisterDto.cs
+++
@@ -1,8 +0,0 @@
-namespace DatingAppV2.API.Dtos
-{
-    public class UserForRegisterDto
-    {
-        public string Username{get;set;}
-        public string Password{get;set;}
-    }
-}
